3f4a457fc2cb2d72d4c6720d1847baa3b4aa39b7
JaySebagh <jaysebagh@gmail.com>
2019-03-20 09:42:51 -0700

    Setup server, added CRUD for posts, started CRUD for users.

---
+++ data/users/users-router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const postdb = require('../helpers/userDb.js');
+
+const router = express.Router();
+
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,16 @@
+// import express
+const express = require('express');
+
+const postsRouter = require('./data/posts/posts-router.js');
+const usersRouter = requrie('./data/users/users-router.js')
+
+// create new http server
+const server = express();
+
+// middleware
+server.use(express.json());
+server.use('/api/posts', postsRouter);
+server.use('/api/users', usersRouter);
+
+// exporting the server
+module.exports = server;
